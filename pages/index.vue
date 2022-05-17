d9c09d9e70144f3d70df0a4561ef1163f6ac6be6
BruhaBruh <drugsho.jaker@gmail.com>
2022-05-17 16:43:37 +0500

    fix background image

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,12 @@
     <div
       class="h-screen relative overflow-hidden flex flex-col items-center justify-center text-gray-50 px-4"
     >
-      <div class="absolute top-0 left-0">
-        <img class="object-cover" src="/img/landing-hero.png" alt="" />
+      <div class="absolute top-0 left-0 h-full w-full">
+        <img
+          class="object-cover min-h-full min-w-full"
+          src="/img/landing-hero.png"
+          alt=""
+        />
         <div
           class="absolute top-0 left-0 bg-gray-900 bg-opacity-50 w-full h-full"
         />
